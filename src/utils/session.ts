2b5a371b6cd5c8c56da4f1fcfbf38f90472794dc
LeoNogueiraVale <leonogueiravale@gmail.com>
2024-12-17 17:11:38 -0300

    first commit

---
+++ src/utils/session.ts
@@ -0,0 +1,28 @@
+export type User = {
+  sub: number;
+  username: string; //id
+  tenantId: string; 
+
+}
+
+
+export function getSession(){
+
+}
+
+
+export function saveSession (token: string){
+  
+
+}
+
+export function destroySession(){
+
+}
+
+
+export function getUser(){
+
+}
+
+
